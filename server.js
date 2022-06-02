378798319474a96a197380c92e3a6414feae0aad
pa3k1999 <patrik1999b@gmail.com>
2022-06-02 20:57:12 +0200

    dodana opcija za stvaranje admina

--- server.js
+++ server.js
@@ -26,21 +26,48 @@ app.use(cors(corsOptions));
 app.get('/novi-user', (req, res) => {
   const email = req.headers['email'];
   const lozinka = req.headers['lozinka'];
-  admin.auth().createUser({
-    email: email,
-    password: lozinka,
-    disabled: false,
-  }).then(async(userCredential) => {
-    console.log('novi user dodan')
-    res.send(userCredential);
-    admin.auth().setCustomUserClaims(userCredential.uid, {premiumAccount: true})
-  })
+  const isAdmin = req.headers['isadmin'];
+  if(isAdmin === 'true'){
+    admin.auth().createUser({
+      email: email,
+      password: lozinka,
+      disabled: false,
+    }).then(async(userCredential) => {
+      console.log('novi user dodan (admin)')
+      res.send(userCredential);
+      admin.auth().setCustomUserClaims(userCredential.uid, {admin: true})
+    })
+  } else {
+    admin.auth().createUser({
+      email: email,
+      password: lozinka,
+      disabled: false,
+    }).then(async(userCredential) => {
+      console.log('novi user dodan (premium)')
+      res.send(userCredential);
+      admin.auth().setCustomUserClaims(userCredential.uid, {premiumAccount: true})
+    })
+  }
+  
 })
 
 app.get('/novi-user-convert', (req, res) => {
     const uid = req.headers['uid'];
     admin.auth().setCustomUserClaims(uid, {premiumAccount: true}).then(console.log('converted'))
   
+})
+
+app.delete('/obrisi-vodica', (req, res) => {
+  const uid = req.headers['uid'];
+  admin.auth().deleteUser(uid)
+  .then(() => {
+    console.log('Vodic obrisan');
+    res.send(true);
+  })
+  .catch((e) => {
+    console.log('Error deleting user:', e);
+    res.send(false);
+  });
 })
 
 // app.get('/server', (req, res) => {
