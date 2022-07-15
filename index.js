7786479f7b69c9a8a77edc779d3128978f9f6adf
pa3k1999 <patrik1999b@gmail.com>
2022-07-15 08:57:36 +0200

    make it better

--- index.js
+++ index.js
@@ -9,18 +9,7 @@ admin.initializeApp({
   databaseURL: "https://evidencija-tura-default-rtdb.europe-west1.firebasedatabase.app"
 });
 
-const allowedOrigins = ['*'];
-
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('hello world')
